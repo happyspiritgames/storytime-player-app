c2276ff41bf440693b89ae791ae8eef50db8c930
The Happy Spirit <dave@happyspiritgames.com>
2018-08-15 15:59:42 -0700

    save for the night

--- src/index.js
+++ src/index.js
@@ -3,7 +3,5 @@ import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.css';
 import './index.css';
 import PlayerApp from './PlayerApp';
-import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<PlayerApp />, document.getElementById('root'));
-registerServiceWorker();
