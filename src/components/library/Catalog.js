2f7c2ed817ac4a47403d6a76d3cf05f185f68d06
The Happy Spirit <dave@happyspiritgames.com>
2018-08-22 08:40:14 -0700

    Improved card layout

--- src/components/library/Catalog.js
+++ src/components/library/Catalog.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
-import { CardGroup, Card, CardBody, CardTitle, CardSubtitle, CardText, Button } from 'reactstrap'
+import { 
+  CardDeck, Card, CardBody, CardTitle, CardSubtitle, CardText, CardFooter, Button, Row 
+} from 'reactstrap'
 import { editionShape } from '../../metadata/dataShapes'
 
 export default class Catalog extends Component {
@@ -23,24 +25,44 @@ export default class Catalog extends Component {
           <CardSubtitle>by {penName}</CardSubtitle>
           <CardText><em>{tagLine}</em></CardText>
           <CardText>{about}</CardText>
-          <Button color="primary" onClick={ () => {play(editionKey)} }>Play</Button>
         </CardBody>
+        <CardFooter>
+          <Button color="primary" onClick={ () => play(editionKey) }>Play</Button>
+        </CardFooter>
       </Card>
+    )
+  }
+
+  renderRow(editions) {
+    const cards = editions.map(edition => this.renderCard(edition.editionKey, edition.summary))
+    // TODO find a way to separate rows verically without having to add extra row
+    return (
+      <div>
+        <Row>
+          {cards}
+        </Row>
+        <Row>&nbsp;</Row>
+      </div>
     )
   }
 
   render() {
     const { editions } = this.props
-    let cards
-    if (editions) {
-      cards = editions.map(edition => this.renderCard(edition.editionKey, edition.summary))
-    } else {
-      cards = []
+    let rows = []
+
+    if (!editions) {
+      return null
+    }
+
+    let next = 0
+    while (editions[next]) {
+      rows.push(this.renderRow(editions.slice(next, next + 3)))
+      next += 3
     }
     return (
-      <CardGroup>
-        {cards}
-      </CardGroup>
+      <CardDeck>
+        {rows}
+      </CardDeck>
     )
   }
 }
--- src/components/library/index.js
+++ src/components/library/index.js
@@ -1,3 +1,50 @@
-import Library from './Library'
+import React, { Component } from 'react'
+import { inject, observer } from 'mobx-react'
+import { withRouter } from 'react-router-dom'
+import { Container } from 'reactstrap'
+import { getPublishedEditions } from '../../api/readerApi'
+import Catalog from './Catalog'
 
-export default Library
+@inject('EditionStore')
+@observer
+class Library extends Component {
+  componentDidMount() {
+    const { EditionStore } = this.props
+    if (!EditionStore.editions || !EditionStore.editions.length) {
+      getPublishedEditions((editions) => EditionStore.setEditions(editions),
+        (error) => console.log(error))
+    }
+  }
+
+  renderEmpty() {
+    return (
+      <Container id="library">
+        <h1>Welcome to the StoryTime Library.</h1>
+        <h3>Loading...</h3>
+        <p>Hold tight while we find some stories to play.</p>
+        <p>If you see this message for more than a few seconds, maybe something is wrong. Lost your connection, perhaps? Or maybe it's us. Sorry about that.</p>
+      </Container>
+    )
+  }
+
+  render() {
+    const { history, EditionStore } = this.props
+
+    const catalogEditions = EditionStore.editions
+    if (!catalogEditions.length) {
+      return this.renderEmpty()
+    }
+
+    const play = (editionKey) => history.push(`/storybook/${editionKey}`)
+
+    return (
+      <Container id="library">
+        <h1>Welcome to the StoryTime Library.</h1>
+        <h3>Find a story to play.</h3>
+        <Catalog key='catalog' editions={catalogEditions} play={play} />
+      </Container>
+    )
+  }
+}
+
+export default withRouter(Library)
